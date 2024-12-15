93474e2af14850ffb8809eeb1bbd14302778a556
eduard0708 <eduard.uytoco@gmail.com>
2024-12-15 11:42:16 +0300

    Update Role

--- Dtos/Admin/EmployeeDto.cs
+++ Dtos/Admin/EmployeeDto.cs
@@ -2,10 +2,12 @@ namespace Pacifica.API.Dtos.Admin
 {
     public class EmployeeDto
     {
-        public string Id { get; set; } = string.Empty;
-        public string EmployeeId { get; set; } = string.Empty;
-        public string? Position { get; set; }
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public List<string>? Roles { get; set; }
         public string? Department { get; set; }
-        public bool IsActive { get; set; } = true;
+        public string? Position { get; set; }
     }
 }
